9087b6a54c4ea34468efe7cec44d524238a36879
tomartushar <tomartushar71@gmail.com>
2024-08-08 09:35:09 +0530

    added redirection command

--- bash_scripting/scripts/34_redirect.sh
+++ bash_scripting/scripts/34_redirect.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# To redirect the output
 ping -c 1 www.google.com > redirect.log
+
+# To redirect the output and error in different file
+ping -c 1 www.google.com > output.log 2>error.log
+
+# To redirect the output and error in the same file
+ping -c 1 www.google.com &> output.log
+
+ping -c 1 www.google.com > output.log 2>&1
